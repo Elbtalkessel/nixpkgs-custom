8547052d3b8edd47f019fdd0c134b7b504eb26ad
Elbtalkessel <rtfsc@pm.me>
2026-01-26 11:07:57 +0100

    Mon 26 Jan 2026 11:07:57 CET

--- packages/tagdb/derivation.nix
+++
@@ -1,7 +0,0 @@
-{ python3Packages }:
-with python3Packages;
-buildPythonApplication {
-  pname = "pytagdb";
-  version = "0.0.1";
-  src = ./src;
-}
--- packages/tagdb/src/README.md
+++
@@ -1,3 +0,0 @@
-## TagDB - Local file database
-
-Tagging files without relying on the file path
