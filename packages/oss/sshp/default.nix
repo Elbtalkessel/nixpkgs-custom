404253c82528ee3c0736f261c0c0ae0b7eb5c168
Elbtalkessel <rtfsc@pm.me>
2026-07-23 09:21:42 +0200

    sshp update to v1.1.4

--- packages/oss/sshp/default.nix
+++ packages/oss/sshp/default.nix
@@ -5,20 +5,14 @@
 }:
 stdenv.mkDerivation rec {
   pname = "sshp";
-  version = "1.1.3";
+  version = "1.1.4";
   src = fetchFromGitHub {
     owner = "bahamas10";
     rev = "v${version}";
     repo = pname;
-    sha256 = "sha256-E7nt+t1CS51YG16371LEPtQxHTJ54Ak+r0LP0erC9Mk=";
+    sha256 = "sha256-4DrNGQQ1ETKuLiB3N+3KnRxx4BEhrCOgskpowbF/KWc=";
   };
   makeFlags = [ "PREFIX=$(out)" ];
-  # In master but not in version 1.1.3
-  preInstall = # bash
-    ''
-      mkdir -p $out/bin
-      mkdir -p $out/man/man1
-    '';
   meta = {
     description = "Parallel SSH Executor";
     homepage = "https://github.com/bahamas10/sshp";
